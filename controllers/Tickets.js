0f137d8514b6cdb95b184ae96238338710ae174e
inganta23 <johanes230500@gmail.com>
2022-06-12 11:56:55 +0700

    Update Backend

--- controllers/Tickets.js
+++ controllers/Tickets.js
@@ -1,29 +1,67 @@
 import Tickets from "../models/TiketModel.js";
+import { Op } from "sequelize";
 
 export const getTickets = async (req, res) => {
-    try {
-      const tickets = await Tickets.findAll({
-        attributes: ["id", "kota_asal", "waktu_berangkat", "kota_tujuan", "waktu_tiba", "nama_kereta", "harga"],
-      });
-      res.json(tickets);
-    } catch (error) {
-      console.log(error);
-    }
-  };
+  try {
+    const tickets = await Tickets.findAll();
+    res.json(tickets);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-  export const insertTickets = async (req, res) => {
-    const { kota_asal, waktu_berangkat, kota_tujuan, waktu_tiba, nama_kereta, harga } = req.body;
-    try {
-      await Tickets.create({
-        kota_asal: kota_asal,
-        waktu_berangkat: waktu_berangkat,
-        kota_tujuan: kota_tujuan,
-        waktu_tiba: waktu_tiba,
-        nama_kereta: nama_kereta,
-        harga: harga
-      });
-      res.json({ msg: "Insert Ticket Berhasil" });
-    } catch (error) {
-      console.log(error);
-    }
-  };
+export const getTicketsById = async (req, res) => {
+  try {
+    const tickets = await Tickets.findAll({
+      where: {
+        id: req.params.id
+      }
+    });
+    res.json(tickets[0]);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const searchTickets = async (req, res) => {
+  const { tanggal_berangkat, kotaAsal, kotaTujuan, namaKereta } = req.body;
+  try {
+    const tickets = await Tickets.findAll({
+      where: {
+        waktu_berangkat: {
+          [Op.gt]: new Date(tanggal_berangkat),
+        },
+        kota_asal: kotaAsal,
+        kota_tujuan: kotaTujuan,
+        nama_kereta: namaKereta
+      }
+    });
+    res.json(tickets);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const insertTickets = async (req, res) => {
+  const {
+    kota_asal,
+    waktu_berangkat,
+    kota_tujuan,
+    waktu_tiba,
+    nama_kereta,
+    harga,
+  } = req.body;
+  try {
+    await Tickets.create({
+      kota_asal: kota_asal,
+      waktu_berangkat: waktu_berangkat,
+      kota_tujuan: kota_tujuan,
+      waktu_tiba: waktu_tiba,
+      nama_kereta: nama_kereta,
+      harga: harga,
+    });
+    res.json({ msg: "Insert Ticket Berhasil" });
+  } catch (error) {
+    console.log(error);
+  }
+};
--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,15 @@ import express from "express";
 import { getUsers, Register, Login, Logout } from "../controllers/Users.js";
 import { verifyToken } from "../middleware/VerifyToken.js";
 import { refreshToken, cekToken } from "../controllers/RefreshToken.js";
-import { getTickets, insertTickets } from "../controllers/Tickets.js";
+import { getTickets, getTicketsById, insertTickets, searchTickets } from "../controllers/Tickets.js";
 
 const router = express.Router();
 
 router.get("/users", verifyToken, getUsers);
 router.get("/tickets", getTickets);
 router.post("/tickets", insertTickets);
+router.post("/searchTickets", searchTickets);
+router.get("/tickets/:id", getTicketsById);
 router.post("/users", Register);
 router.post("/login", Login);
 router.get("/token", refreshToken);
